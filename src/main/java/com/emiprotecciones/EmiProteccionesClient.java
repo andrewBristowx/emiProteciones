63a214284f3baa96c2a7c63da4c53af77832db3c
andrewBristowx <138131314+andrewBristowx@users.noreply.github.com>
2026-08-08 19:08:33 -0500

    Add dedicated client GeckoLib renderer entrypoint

---
+++ src/main/java/com/emiprotecciones/EmiProteccionesClient.java
@@ -0,0 +1,31 @@
+package com.emiprotecciones;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib.model.DefaultedBlockGeoModel;
+import software.bernie.geckolib.renderer.GeoBlockRenderer;
+
+public final class EmiProteccionesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        BlockEntityRendererFactories.register(
+                EmiProtecciones.PROTECTION_CORE_BLOCK_ENTITY,
+                context -> new ProtectionCoreRenderer()
+        );
+    }
+
+    public static final class ProtectionCoreModel
+            extends DefaultedBlockGeoModel<EmiProtecciones.ProtectionCoreBlockEntity> {
+        public ProtectionCoreModel() {
+            super(Identifier.of(EmiProtecciones.MOD_ID, "protection_core"));
+        }
+    }
+
+    public static final class ProtectionCoreRenderer
+            extends GeoBlockRenderer<EmiProtecciones.ProtectionCoreBlockEntity> {
+        public ProtectionCoreRenderer() {
+            super(new ProtectionCoreModel());
+        }
+    }
+}
